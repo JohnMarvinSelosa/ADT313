5b59aeabf6f1f1c0497378bff87c5ce5edcfa958
John Marvin Selosa <selosajohnmarvin11@gmail.com>
2024-08-31 06:00:36 +0800

    hands on1

---
+++ hands-on1-3a/src/component/Description/Description.jsx
@@ -0,0 +1,11 @@
+import './Description.css';
+function Description({description}) {
+    return (
+        <div>
+            <p className='text-red'>{description}</p>
+        </div>
+    )
+    
+}
+
+export default Description;
